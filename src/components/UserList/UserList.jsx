44b959331ac677cef4341b698bcf7a120ed968f4
6urrp <sndrtasic@gmail.com>
2021-08-20 15:45:17 +0200

    User list exercise modified using stateful component to fetch data, UserList.jsx component changed from functional to class

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { Component } from "react";
 
-import { data } from "../data/AllUsers"
+import { getUsers } from "../data/AllUsers"
 import { Avatar } from "../Avatar/Avatar";
 import { UserInfo } from "../UserInfo/UserInfo";
 import { ListButton } from "../ListButton/ListButton";
@@ -9,43 +9,95 @@ import { GridButton } from "../GridButton/GridButton";
 import "./UserList.css"
 import { UserListGrid } from "../UserListGrid/UserListGrid";
 
-const UserList = () => {
+// const UserList = () => {
 
-    const [selected, setSelected] = useState(true);
+//     const [selected, setSelected] = useState(true);
 
-    const onListClick = () => {
-        setSelected(true);
+//     const onListClick = () => {
+//         setSelected(true);
+//     }
+
+//     const onGridClick = () => {
+//         setSelected(false);
+//     }
+
+//     return (
+//         <div className="main-container">
+//             <div className="btn-container">
+//                 <ListButton onClick={onListClick}/>
+//                 <GridButton onClick={onGridClick}/>
+//             </div>
+//             <div class="flex-container">
+//             {
+//             data.results.map((user, index) => !selected 
+//                 ? (
+//                 <div className="single-grid-user" key={index}>
+//                     <UserListGrid userGridData={user}/>
+//                 </div>
+//                 )
+//                 : 
+//                 (
+//                 <div className="single-user" key={index}>
+//                     <Avatar userLogo={user}/>
+//                     <UserInfo userData={user}/>
+//                 </div>
+//                 ))
+//             }
+//             </div>
+//         </div>
+//     )
+// };
+
+
+class UserList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected: true,
+            users: []
+        }
     }
 
-    const onGridClick = () => {
-        setSelected(false);
+    onListClick = () => this.setState({selected: true})
+
+    onGridClick = () => this.setState({selected: false})
+
+
+    componentDidMount () {
+        getUsers()
+            .then(users => {
+                this.setState({users: users})
+            })
     }
 
-    return (
-        <div className="main-container">
-            <div className="btn-container">
-                <ListButton onClick={onListClick}/>
-                <GridButton onClick={onGridClick}/>
-            </div>
-            <div class="flex-container">
-            {
-            data.results.map((user, index) => !selected 
-                ? (
-                <div className="single-grid-user" key={index}>
-                    <UserListGrid userGridData={user}/>
+    render () {
+        return (
+            <div className="main-container">
+                <div className="btn-container">
+                    <ListButton onClick={this.onListClick}/>
+                    <GridButton onClick={this.onGridClick}/>
                 </div>
-                )
-                : 
-                (
-                <div className="single-user" key={index}>
-                    <Avatar userLogo={user}/>
-                    <UserInfo userData={user}/>
+                <div className="flex-container">
+                {
+                this.state.users.map((user, index) => !(this.state.selected) 
+                    ? (
+                    <div className="single-grid-user" key={index}>
+                        <UserListGrid userGridData={user}/>
+                    </div>
+                    )
+                    : 
+                    (
+                    <div className="single-user" key={index}>
+                        <Avatar userLogo={user}/>
+                        <UserInfo userData={user}/>
+                    </div>
+                    ))
+                }
                 </div>
-                ))
-            }
             </div>
-        </div>
-    )
-};
+        )
+        
+    }
+}
 
 export { UserList };
